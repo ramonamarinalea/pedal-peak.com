43a9f7bc747156de837002d4eff9dc4976110535
ramonamarinalea <ramona.furter@gmail.com>
2025-06-20 19:04:14 +0200

    Initial commit from Create Next App

---
+++ src/components/theme-switcher.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { ComponentProps } from "react";
+
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+type ThemeSwitcherProps = {
+  className?: ComponentProps<"button">["className"];
+};
+
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className={cn("cursor-pointer", className)}
+      size="icon"
+      variant="outline"
+      aria-label="Toggle theme"
+    >
+      <Icons.sun className="dark:hidden" />
+      <Icons.moon className="hidden dark:block" />
+    </Button>
+  );
+};
